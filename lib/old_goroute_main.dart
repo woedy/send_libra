92363c41350ffde4de6e76c5273686ff9d15a5e5
SamaWyna <etornamasamoah@gmail.com>
2023-10-23 11:07:31 +0000

    Working Phase 2.
    - Functioning App with go route implemented.
    - Go route not yet connected to world pay but working..

--- lib/old_goroute_main.dart
+++ lib/old_goroute_main.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:send_libra/Screens/Authentication/SignUp/sign_up_resend_verification.dart';
-import 'package:send_libra/Screens/Authentication/SignUp/sign_up_user_info2.dart';
-import 'package:send_libra/Screens/SendMoney/SendNormal/transaction_complate_screen.dart';
-
+import 'package:go_router/go_router.dart';
 
 import 'package:send_libra/Screens/SplashScreen/splash_screen.dart';
 
 import 'package:send_libra/constants.dart';
-import 'package:send_libra/test_home.dart';
 
-import 'Components/theme.dart';
-import 'Screens/Authentication/SignUp/sign_up_verification.dart';
 import 'Screens/HomeScreen/HomeScreen.dart';
-import 'Screens/SendMoney/SendNormal/BankTransfer/bank_transfer_screen.dart';
-import 'Screens/SendMoney/SendNormal/Card/credit_card_info_screen.dart';
-import 'playground2.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,15 +28,27 @@ class MyApp extends StatelessWidget {
         // Hide the keyboard when tapping outside the text field
         FocusScope.of(context).requestFocus(FocusNode());
       },
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Libra',
-        theme: theme(),
-        home: MyHomePage(),
-      ),
+      child: MaterialApp.router(routerConfig: router,),
     );
   }
 }
+
+final router = GoRouter(routes: [
+  GoRoute(path: '/',
+  builder: (_,__)=> Scaffold(
+    appBar: AppBar(title: Text("Dashboard"),),
+  ),
+    routes: [
+      GoRoute(path: 'profile',
+      builder: (_,__)=>Scaffold(
+        appBar: AppBar(title: const Text("Profile"),),
+      ))
+    ]
+
+  ),
+
+
+]);
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
